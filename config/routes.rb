9ddf4874fdcf1db7ec1109bde487c6ee9a245e30
EC2 Default User <ec2-user@ip-172-31-34-252.ec2.internal>
2020-08-15 08:47:32 +0000

    7.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+      # トップページにアクセスしたときのルーティングを
+      #  TasksControllerコントローラのindexアクションに設定する　つまり、
+      #  / （トップページ） にアクセスしたときと、 /messages にアクセスした両方で同じルーティングが設定された
+  root to: 'tasks#index'
+  
+  
+  # get 'tasks/:id', to: 'tasks#show'
+  # post 'tasks', to: 'tasks#create'
+  # put 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  
+  # get 'tasks', to: 'tasks#index'
+  
+  # get 'tasks/new', to: 'tasks#new'
+  
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  
+  resources :tasks   # 7つのルーティングの省略形
+  
 end
