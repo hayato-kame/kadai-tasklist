736cc4fb3f2e6b5e446aa1fbb5d13a6fa183c7fb
EC2 Default User <ec2-user@ip-172-31-34-252.ec2.internal>
2020-08-19 01:17:28 +0000

    9.2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,19 @@
 class Task < ApplicationRecord
   
+  belongs_to :user
+  
   validates :content, presence: true, length: { maximum: 255 }
   
   validates :status, presence: true, length: { maximum: 10 }
   
 end
+
+# belongs_to :user の関連付けによって、
+# ユーザの紐付け無しには Taskを保存できません。
+# belongs_to :user の関連付けによって、
+# Task のインスタンスでインスタンスメソッド user が使えるようになります。
+
+# 使用例：task.user
+
+# このuserメソッドで、
+# ある投稿に紐付いた User のインスタンスを取得することができます。
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
+  
+  has_many :tasks
 end
+
+# User に設定した has_many :taks の関連付けによって、
+# User のインスタンスでインスタンスメソッド tasks が使えるようになります。
+
+# 使用例：user.tasks
+
+# このtasksメソッドで、
+# ある User が投稿した Tasks を全件取得することができます。
