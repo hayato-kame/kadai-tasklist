67a6d77e9fe4cffb4680cd386165c428c5d4694b
EC2 Default User <ec2-user@ip-172-31-34-252.ec2.internal>
2020-08-19 11:19:59 +0000

    kadai3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,7 @@ class TasksController < ApplicationController
   
  before_action :require_user_logged_in, only: [:index, :show]
  
- before_action :set_task, only: [:show, :edit, :update, :destroy]
- 
- before_action :correct_user, only: [:show, :update, :destroy]  
+ before_action :correct_user, only: [:show, :edit, :update, :destroy]  
   
   
   def index
@@ -54,9 +52,6 @@ class TasksController < ApplicationController
 
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   #Strong Parameter
   def task_params
